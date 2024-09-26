bb141a2c31dd016dfd23fa6abeeb6d552765442f
Raghavendra T Kalli <59082243+raghutkalli@users.noreply.github.com>
2024-09-26 13:29:49 +0530

    Update streamlit_app.py
    Expose the JSON Data Inside the Response Object

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
+# New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+# st.text(fruityvice_response.json)
+fv_df= st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
         
