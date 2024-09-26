3b24152644ebef17e4509a5e01cbe3d3b8d3e4e8
Raghavendra T Kalli <59082243+raghutkalli@users.noreply.github.com>
2024-09-26 13:40:21 +0530

    Update streamlit_app.py
    indentation carriedout

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie!")
@@ -29,6 +30,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df= st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -43,11 +46,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
-# New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json)
-fv_df= st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
-        
+        st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")        
